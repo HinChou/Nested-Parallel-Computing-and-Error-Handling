508cb9393608992df30fb6956191c6868f5bdb16
Xuan Zhou <hinn.chou@gmail.com>
2016-06-25 13:31:51 -0400

    Add files via upload
    Implementing the parallel computing in R is quite easy, although we know it should avoid to using loop in R.
    In the case, we have to loop in large data sets, the parallel computing can save a lot of time.
    
    For nested parallel computing, there is always  a question of which loop to parallelize. The standard advice is to parallelize the outer loop. However, if the outer loop doesn't have many iterations, programmer can parallelize the inner loop instead.
    
    Another thing in R is there is  a nest loop operator, "%:%". This operator can change two loop into one. But it is not prefect right now. I mean sometimes the program still runs in sequentially instead of in parallel way.

---
+++ Nested Parallel Pseudocode.R
@@ -0,0 +1,27 @@
+library(doParallel)
+
+# Create a user-built function
+Fun <- function()
+{
+  "..."
+}
+
+# Create cluster with desired number of cores, in this example is 8
+cluster <- makeCluster(8)
+
+# Register cluster
+registerDoParallel(cluster)
+
+# Returns an object, a list by default. This can be changed through the ".combine" option
+# For nested parallel, you have to load "foreach" package into each parrelel worker(core)
+result <- foreach(i = "...", .combine = "...", .packages = "foreach") %dopar{
+  foreach(j = "...") %do% {
+    tryCatch(Fun(), error = function(e){"error-handleer-code"}) 
+  }
+}
+
+# Be sure to close the parallel backend, otherwise you can run into problems later
+stopCluster(cl)
+
+
+
